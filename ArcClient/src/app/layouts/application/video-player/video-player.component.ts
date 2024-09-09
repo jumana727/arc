c0ad904b72c3baa17ffaa05d0f79cf375a17f9ba
Sharan Phadke <sharan.phadke@matrixcomsec.com>
2024-09-09 09:52:47 +0530

    fix: hls url accessible from local network

--- ArcClient/src/app/layouts/application/video-player/video-player.component.ts
+++ ArcClient/src/app/layouts/application/video-player/video-player.component.ts
@@ -398,7 +398,7 @@ export class VideoPlayerComponent implements OnInit, OnDestroy {
     if (this.isHLS === true)
     {
       this.hlsURL = this.webRTCURL.replace(':8889', '');
-      this.hlsURL = this.hlsURL.replace('mediamtx.default.svc.cluster.local', 'hls-streams');
+      this.hlsURL = this.hlsURL.replace('mediamtx.default.svc.cluster.local', 'hls-streams:4200');
 
       // this.hlsURL = "http://hls-streams/matrixcamera/";
       if (Hls.isSupported()) {
