0a3da6449ecb7cbd09bd5b6462a4a19df35b87bf
Arjun Khunt <arjun.khunt@matrixcomsec.com>
2024-10-10 09:49:41 +0530

    feat(arcClient): add app update handling to PWA

--- ArcClient/src/app/app.component.ts
+++ ArcClient/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { IconSetService } from '@coreui/icons-angular';
 import { iconSubset } from './shared/icons/icon-subset';
 import { PageTitleService } from './layouts/shared/services/pageTitle/page-title.service';
+import { AppUpdateService } from './layouts/shared/services/appUpdate/app-update.service';
 
 @Component({
   selector: 'app-root',
@@ -15,8 +16,8 @@ export class AppComponent implements OnInit {
   constructor(
     private router: Router,
     private pageTitleService : PageTitleService,
-    private iconSetService: IconSetService
-  ) {
+    private iconSetService: IconSetService,
+    private appUpdateService: AppUpdateService) {
     this.pageTitleService.setTitle(undefined);
     // iconSet singleton
     this.iconSetService.icons = { ...iconSubset };
